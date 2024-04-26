77799761a29e27260863bd6cced876900cce4a38
pgl <501143194@qq.com>
2024-04-26 13:23:35 +0800

    补充Mock注解说明

--- springboot-junit/src/test/java/com/pgl/service/UserServiceTest.java
+++ springboot-junit/src/test/java/com/pgl/service/UserServiceTest.java
@@ -12,8 +12,6 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
 
 /**
  * @author pgl
@@ -23,11 +21,11 @@ import static org.mockito.Mockito.mock;
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
-    @Spy
-    @InjectMocks
+    @Spy // 创建真实的对象实例，并允许模拟该对象的一些行为（当你想要保留对象的一些实际行为，同时又想模拟它的某些方法时，@Spy就非常有用。）
+    @InjectMocks // 创建被测试类的实例时，自动注入被@Mock和@Spy注解创建的模拟对象或间谍对象
     private UserServiceImpl userServiceSpy;
 
-    @Mock
+    @Mock // 创建模拟的对象实例
     private RoleService roleService;
 
     @Test
@@ -35,7 +33,9 @@ public class UserServiceTest {
         UserPojo userPojo = new UserPojo();
         userPojo.setId(155L);
         userPojo.setName("name1111");
-        // 模拟调用并指定返回值（两种方法区别：Mockito.doReturn()不会调用真实方法，Mockito.when()会调用真实方法）
+        // 模拟调用并指定返回值，两种方法区别：
+        // Mockito.doReturn()不会调用实际方法，
+        // Mockito.when()会调用实际方法
         Mockito.doReturn(userPojo).when(userServiceSpy).findUserById(any(Long.class));
         // Mockito.when(userServiceSpy.findUserById(155L)).thenReturn(userPojo);
 
