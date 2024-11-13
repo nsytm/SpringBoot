53e13a6eba202152f58667ceaddaed5cccc8bd57
nsytm <501143194@qq.com>
2024-11-13 17:33:56 +0800

    新增模块 spring-boot-java

---
+++ spring-boot-java/src/main/java/com/example/SpringbootJavaApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 天纵神威
+ * @date 2024/11/13
+ * @description SpringBoot 启动类
+ */
+@SpringBootApplication
+public class SpringbootJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJavaApplication.class, args);
+    }
+
+}
